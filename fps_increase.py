f8a6879650342a7e54a24a3c930d398f45f2bb1a
Adam Selker <adamselker@gmail.com>
2019-05-06 23:40:04 -0400

    Add crunchbang, +x, format

--- fps_increase.py
+++ fps_increase.py
@@ -1,74 +1,78 @@
+#!/usr/bin/env python3
 # import the necessary packages
 import datetime
+
 # import the necessary packages
 from threading import Thread
 import cv2
 import imutils
 
+
 class FPS:
-	def __init__(self):
-		# store the start time, end time, and total number of frames
-		# that were examined between the start and end intervals
-		self._start = None
-		self._end = None
-		self._numFrames = 0
+    def __init__(self):
+        # store the start time, end time, and total number of frames
+        # that were examined between the start and end intervals
+        self._start = None
+        self._end = None
+        self._numFrames = 0
 
-	def start(self):
-		# start the timer
-		self._start = datetime.datetime.now()
-		return self
+    def start(self):
+        # start the timer
+        self._start = datetime.datetime.now()
+        return self
 
-	def stop(self):
-		# stop the timer
-		self._end = datetime.datetime.now()
+    def stop(self):
+        # stop the timer
+        self._end = datetime.datetime.now()
 
-	def update(self):
-		# increment the total number of frames examined during the
-		# start and end intervals
-		self._numFrames += 1
+    def update(self):
+        # increment the total number of frames examined during the
+        # start and end intervals
+        self._numFrames += 1
 
-	def elapsed(self):
-		# return the total number of seconds between the start and
-		# end interval
-		return (self._end - self._start).total_seconds()
+    def elapsed(self):
+        # return the total number of seconds between the start and
+        # end interval
+        return (self._end - self._start).total_seconds()
 
-	def fps(self):
-		# compute the (approximate) frames per second
-		return self._numFrames / self.elapsed()
+    def fps(self):
+        # compute the (approximate) frames per second
+        return self._numFrames / self.elapsed()
+
 
 class WebcamVideoStream:
-	def __init__(self, src=0):
-		# initialize the video camera stream and read the first frame
-		# from the stream
-		self.stream = cv2.VideoCapture(src)
-		(self.grabbed, self.frame) = self.stream.read()
+    def __init__(self, src=0):
+        # initialize the video camera stream and read the first frame
+        # from the stream
+        self.stream = cv2.VideoCapture(src)
+        (self.grabbed, self.frame) = self.stream.read()
 
-		# initialize the variable used to indicate if the thread should
-		# be stopped
-		self.stopped = False
+        # initialize the variable used to indicate if the thread should
+        # be stopped
+        self.stopped = False
 
-	def start(self):
-		# start the thread to read frames from the video stream
-		Thread(target=self.update, args=()).start()
-		return self
+    def start(self):
+        # start the thread to read frames from the video stream
+        Thread(target=self.update, args=()).start()
+        return self
 
-	def update(self):
-		# keep looping infinitely until the thread is stopped
-		while True:
-			# if the thread indicator variable is set, stop the thread
-			if self.stopped:
-				return
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        while True:
+            # if the thread indicator variable is set, stop the thread
+            if self.stopped:
+                return
 
-			# otherwise, read the next frame from the stream
-			(self.grabbed, self.frame) = self.stream.read()
+            # otherwise, read the next frame from the stream
+            (self.grabbed, self.frame) = self.stream.read()
 
-	def read(self):
-		# return the frame most recently read
-		return self.frame
+    def read(self):
+        # return the frame most recently read
+        return self.frame
 
-	def stop(self):
-		# indicate that the thread should be stopped
-		self.stopped = True
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
 
 
 num_frames = 400
@@ -83,24 +87,24 @@ fps = FPS().start()
 
 # loop over some frames...this time using the threaded stream
 while fps._numFrames < num_frames:
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
-	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
- 	
-	# check to see if the frame should be displayed to our screen
-	if display > 0:
-		cv2.imshow("Frame", frame)
-		key = cv2.waitKey(1) & 0xFF
- 
-	# update the FPS counter
-	fps.update()
- 
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 400 pixels
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
+
+    # check to see if the frame should be displayed to our screen
+    if display > 0:
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+    # update the FPS counter
+    fps.update()
+
 # stop the timer and display FPS information
 fps.stop()
 print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
 print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
- 
+
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
